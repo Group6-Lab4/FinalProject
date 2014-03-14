a9279859df9dce30038bc2d29dd844074f23c856
Gigi <gigiho609@gmail.com>
2014-03-14 05:19:06 +0100

    [JS] added all view js, utils.js
    - moved storyModel.js to /model
    [app.js]
    - added all goto functions
    - added goto page according to params

---
+++ js/view/orderView.js
@@ -0,0 +1,31 @@
+/* 
+ * @author Gigi Ho
+ * 
+ */
+
+var OrderView = function(container, model) {
+
+	this.showView = function() {
+		$(container).show();
+	};
+	this.hideView = function(){
+		$(container).hide();
+	};
+
+
+
+
+	/*****************************************  
+	 Observer implementation    
+	 *****************************************/
+	//Register an observer to the model
+	model.addObserver(this);
+
+	//This function gets called when there is a change at the model
+	this.update = function(arg) {
+//        alert("Changed data: " + arg);
+		this.updateSelectedDishes();
+	};
+};
+
+
