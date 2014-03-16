82cf2d9d1dae9b0fd3fd92f317a11ca0f368f6ef
Gigi <gigiho609@gmail.com>
2014-03-17 00:11:39 +0100

    start to implement draggable and droppable

--- js/controller/editViewController.js
+++ js/controller/editViewController.js
@@ -4,7 +4,21 @@
  */
 
 var EditViewController = function(view, model) {
-	
-	
+	$(".draggable_item").draggable({
+		helper: 'clone',
+		containment: 'frame'
+//		cursor: "move" 
+	});
+
+
+var droppableCount = 0;
+	$("#droppable_canvas").droppable({
+		drop: function(event, ui) {
+			droppableCount++;
+			$(this).html("Dropped!" + droppableCount);
+		}
+	});
+
+
 };
 
