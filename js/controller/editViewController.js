e30b51a5eaaa555253a8cd053c5ec80a8d4fb638
weiweizh <weiweizh@kth.se>
2014-03-16 23:34:12 +0100

    -remove the default stretched out style of leftside bar when mouseout or click other item

--- js/controller/editViewController.js
+++ js/controller/editViewController.js
@@ -4,6 +4,12 @@
  */
 
 var EditViewController = function(view, model) {
+    
+    /*remove the defulte streched out style of left menu when click or mouse over*/
+    $(".catgories").on("click mouseout", function(){
+        $("#icon_bg div").attr("id","cat_bgs");
+        $("#icon_bg img").attr("id","");
+    });
 	
 	
 };
