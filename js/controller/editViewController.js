7bbd9632d2f9859fb27f5e73b911fb38c0ed62e4
Gigi <gigiho609@gmail.com>
2014-03-17 03:04:01 +0100

    [drag-n-drop] Implemented basic dragndrop
    - modified sidebar menu css for drag-n-drop
    - added css for drag-n-drop
    - added js in editViewController.js for drag-n-drop

--- js/controller/editViewController.js
+++ js/controller/editViewController.js
@@ -4,24 +4,43 @@
  */
 
 var EditViewController = function(view, model) {
-    /*remove the defulte streched out style of left menu when click or mouse over*/
-    $(".catgories").on("click mouseout", function(){
-        $("#icon_bg div").attr("id","cat_bgs");
-        $("#icon_bg img").attr("id","");
-    });
-	
+	/*remove the defulte streched out style of left menu when click or mouse over*/
+	$(".catgories").on("click mouseout", function() {
+		$("#icon_bg div").attr("id", "cat_bgs");
+		$("#icon_bg img").attr("id", "");
+	});
+
 	$(".draggable_item").draggable({
 		helper: 'clone',
-		containment: 'frame'
+		containment: 'document',
+		delay: 0,
+		grid: false,
+//		opacity: 0.5,
+//		refreshPositions: true
 //		cursor: "move" 
 	});
 
 
-var droppableCount = 0;
 	$("#droppable_canvas").droppable({
 		drop: function(event, ui) {
-			droppableCount++;
-			$(this).html("Dropped!" + droppableCount);
+			var element = $(ui.draggable).clone();
+			
+			var draggablePos = $(ui.helper).offset();
+			var canvasPos = $(this).offset();
+			
+			var relPosInPercent = {
+				'left': (draggablePos.left - canvasPos.left)/$(this).width() * 100,
+				'top' : (draggablePos.top - canvasPos.top)/$(this).height() * 100
+			};
+			
+            $(element).css({
+                "left": relPosInPercent.left + "%",
+                "top": relPosInPercent.top + "%"
+            });
+			
+			$(this).append(element);
+			
+			
 		}
 	});
 
